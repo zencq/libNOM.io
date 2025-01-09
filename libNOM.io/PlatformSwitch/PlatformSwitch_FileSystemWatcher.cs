c29b8ee7a22aec3a103ccce252fa03f295f90650
Christian Engelhardt <cengelha.appdevl@outlook.com>
2025-01-09 16:58:32 +0100

    refactor(Platform): always split into files (part 3)

---
+++ libNOM.io/PlatformSwitch/PlatformSwitch_FileSystemWatcher.cs
@@ -0,0 +1,20 @@
+﻿namespace libNOM.io;
+
+
+/// <summary>
+/// Implementation for the Nintendo Switch platform.
+/// </summary>
+// This partial class contains FileSystemWatcher related code.
+public partial class PlatformSwitch : Platform
+{
+    // Accessor
+
+    #region Getter
+
+    protected override IEnumerable<Container> GetCacheEvictionContainers(string name)
+    {
+        return SaveContainerCollection.Where(i => i.MetaFile?.Name.Equals(name, StringComparison.OrdinalIgnoreCase) == true);
+    }
+
+    #endregion
+}
---
+++ libNOM.io/PlatformSwitch/PlatformSwitch_Property.cs
@@ -0,0 +1,51 @@
+﻿namespace libNOM.io;
+
+
+/// <summary>
+/// Implementation for the Nintendo Switch platform.
+/// </summary>
+// This partial class contains property related code.
+public partial class PlatformSwitch : Platform
+{
+    #region Flags
+
+    // public //
+
+    public override bool CanCreate { get; } = true;
+
+    public override bool CanRead { get; } = true;
+
+    public override bool CanUpdate { get; } = true;
+
+    public override bool CanDelete { get; } = true;
+
+    public override bool HasModding { get; } = false;
+
+    public override bool IsValid => AnchorFileIndex == 0; // { get; }
+
+    public override RestartRequirementEnum RestartToApply { get; } = RestartRequirementEnum.Always;
+
+    // protected //
+
+    protected override bool IsConsolePlatform { get; } = true;
+
+    #endregion
+
+    #region Platform Indicator
+
+    // public //
+
+    public override PlatformEnum PlatformEnum { get; } = PlatformEnum.Switch;
+
+    // protected //
+
+    protected override string[] PlatformAnchorFilePattern { get; } = ANCHOR_FILE_PATTERN;
+
+    protected override string? PlatformArchitecture { get; } = "NX1|Final";
+
+    protected override string? PlatformProcessPath { get; } = null;
+
+    protected override string PlatformToken { get; } = "NS";
+
+    #endregion
+}
